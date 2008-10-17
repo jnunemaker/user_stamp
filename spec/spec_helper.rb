2ef9f29cda7a4a47dce64124b0b55b12b5c080a6
John Nunemaker <nunemaker@gmail.com>
2008-10-17 11:37:38 -0400

    Added the user stamp files. Updated README and install.rb.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,24 @@
+require 'rubygems'
+
+gem 'rspec'
+require 'spec'
+
+%w[activesupport activerecord actionpack].each do |lib|
+  gem lib
+  require lib
+end
+
+require 'action_controller'
+
+$:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
+require 'user_stamp'
+
+UserStampSweeper.instance
+
+class User
+  attr_accessor :id
+  
+  def initialize(id);
+    @id = id
+  end
+end
